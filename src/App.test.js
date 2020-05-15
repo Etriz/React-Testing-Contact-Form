26e80dd8ceccac82e1124f10b35535c3fbd75e85
ryanp <ryanpanimation@gmail.com>
2020-05-15 00:25:56 -0500

    successful test

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,38 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
+// import user from "@testing-library/user-event";
 import App from "./App";
+
+// const setup = () => {
+//   const app = render(<App />);
+//   const { getByTestId, getByRole, findByTestId } = render(<App />);
+// };
 
 test("renders App without crashing", () => {
   render(<App />);
+});
+test("submits form successfully", async () => {
+  const { getByTestId, getByRole, findByTestId } = render(<App />);
+
+  const firstName = getByTestId("firstName");
+  const lastName = getByTestId("lastName");
+  const email = getByTestId("email");
+  const message = getByTestId("message");
+  const submitBtn = getByRole("button", { name: /submit/i });
+
+  fireEvent.change(firstName, { target: { value: "Ryan" } });
+  expect(firstName.value).toBe("Ryan");
+
+  fireEvent.change(lastName, { target: { value: "Paulson" } });
+  expect(lastName.value).toBe("Paulson");
+
+  fireEvent.change(email, { target: { value: "email@email.com" } });
+  expect(email.value).toBe("email@email.com");
+
+  fireEvent.change(message, { target: { value: "message" } });
+  expect(message.value).toBe("message");
+
+  fireEvent.click(submitBtn);
+
+  await expect((await findByTestId("preTag")).textContent).toContain("email@email.com");
 });
--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -12,46 +12,42 @@ const ContactForm = () => {
 
   return (
     <div className="App">
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <form onSubmit={handleSubmit(onSubmit)} data-testid="form">
         <div>
           <label htmlFor="firstName">First Name*</label>
           <input
+            data-testid="firstName"
             name="firstName"
             placeholder="Edd"
-            ref={register({ required: true, maxLength: 3 })}
+            ref={register({ required: true, minLength: 2 })}
           />
-          {errors.firstName && (
-            <p>Looks like there was an error: {errors.firstName.type}</p>
-          )}
+          {errors.firstName && <p>Looks like there was an error: {errors.firstName.type}</p>}
         </div>
 
         <div>
           <label htmlFor="lastName">Last Name*</label>
           <input
+            data-testid="lastName"
             name="lastName"
             placeholder="Burke"
             ref={register({ required: true })}
           />
-          {errors.lastName && (
-            <p>Looks like there was an error: {errors.lastName.type}</p>
-          )}
+          {errors.lastName && <p>Looks like there was an error: {errors.lastName.type}</p>}
         </div>
 
         <div>
           <label htmlFor="email" placeholder="bluebill1049@hotmail.com">
             Email*
           </label>
-          <input name="email" ref={register({ required: true })} />
-          {errors.email && (
-            <p>Looks like there was an error: {errors.email.type}</p>
-          )}
+          <input data-testid="email" name="email" ref={register({ required: true })} />
+          {errors.email && <p>Looks like there was an error: {errors.email.type}</p>}
         </div>
         <div>
           <label htmlFor="message">Message</label>
-          <textarea name="message" ref={register({ required: false })} />
+          <textarea data-testid="message" name="message" ref={register({ required: false })} />
         </div>
         {data && (
-          <pre style={{ textAlign: "left", color: "white" }}>
+          <pre data-testid="preTag" style={{ textAlign: "left", color: "white" }}>
             {JSON.stringify(data, null, 2)}
           </pre>
         )}
